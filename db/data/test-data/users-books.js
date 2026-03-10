cb644e0657fdd7667d226ee1be23f97599c75dc2
gavinhousley <gavinhousley@gmail.com>
2026-03-10 15:11:40 +0000

    updated users-books data

--- db/data/test-data/users-books.js
+++ db/data/test-data/users-books.js
@@ -3,59 +3,80 @@ module.exports = [
     isbn: "9780679723394",
     username: jovaScript,
   },
-
   {
     isbn: "9781608464456",
     username: jovaScript,
   },
-
   {
     isbn: "9781784870911",
     username: jovaScript,
   },
-
   {
     isbn: "9780441569595",
     username: jovaScript,
   },
-
   {
     isbn: "9780140154511",
     username: gavinHousley,
   },
-
   {
     isbn: "9780441627400",
     username: gavinHousley,
   },
-
   {
     isbn: "9780374530068",
     username: gavinHousley,
   },
-
   {
     isbn: "9780393312836",
     username: gavinHousley,
   },
-
   {
     isbn: "9780393312836",
     username: coolSurferDude,
   },
-
   {
     isbn: "9780307887443",
     username: coolSurferDude,
   },
-
   {
     isbn: "9780618640157",
     username: coolSurferDude,
   },
-
   {
     isbn: "9780393312836",
     username: coolSurferDude,
+  },
+  {
+    isbn: "9780441569595",
+    username: nomi,
+  },
+  {
+    isbn: "9780140154511",
+    username: nomi,
+  },
+  {
+    isbn: "9780441627400",
+    username: nomi,
+  },
+  {
+    isbn: "9780618640157",
+    username: nomi,
+  },
+  {
+    isbn: "9780441569595",
+    username: elenaS,
+  },
+  {
+    isbn: "9781567923384",
+    username: elenaS,
+  },
+  {
+    isbn: "9780441627400",
+    username: elenaS,
+  },
+  {
+    isbn: "9781911547860",
+    username: elenaS,
   },
 ];
