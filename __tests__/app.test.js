a3b622687b83dd7c9159a49b7e056a23a4b397e5
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-03-10 15:04:20 +0000

    added seed.test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest");
-const app = require("../app"); // Point to your app.js
+const app = require("../app");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 
-beforeEach(() => seed(data));
+// beforeEach(() => seed());
 
-afterAll(() => db.end());
+// afterAll(() => db.end());
 
 describe("test", () => {
   test("test", () => {
