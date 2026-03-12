83e6649569744e2e973e15005a9f675847cdac4b
J-Mro <j.k.mroueh@gmail.com>
2026-03-12 12:26:52 +0000

    add serviceGetWishListByUsername method

--- service/users.service.js
+++ service/users.service.js
@@ -2,6 +2,7 @@ const {
   modelGetUsers,
   modelGetBooksByUsername,
   modelGetFriendsByUsername,
+  modelGetWishListByUsername,
 } = require("../model/users.model");
 
 exports.serviceGetUsers = async () => {
@@ -14,4 +15,8 @@ exports.serviceGetBooksByUsername = async (username) => {
 
 exports.serviceGetFriendsByUsername = async (username) => {
   return await modelGetFriendsByUsername(username);
+};
+
+exports.serviceGetWishListByUsername = async (username) => {
+  return await modelGetWishListByUsername(username);
 };
