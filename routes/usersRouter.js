8b4bd6327324c8332f3b7d907e14d7c685ca5f22
J-Mro <j.k.mroueh@gmail.com>
2026-03-12 11:25:31 +0000

    add /:username/friends route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,9 +3,11 @@ const usersbooksRouter = express.Router({ mergeParams: true });
 const {
   controllerGetUsers,
   controllerGetBooksByUsername,
+  controllerGetFriendsByUsername,
 } = require("../controller/users.controller");
 
 usersbooksRouter.get("/", controllerGetUsers);
 usersbooksRouter.get("/:username/my-library", controllerGetBooksByUsername);
+usersbooksRouter.get("/:username/friends", controllerGetFriendsByUsername);
 
 module.exports = usersbooksRouter;
