fd5f8f0f331d71382eefc03854e4ae650cebaeeb
J-Mro <j.k.mroueh@gmail.com>
2026-03-12 12:26:30 +0000

    add controllerGetWishListByUsername method

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -2,6 +2,7 @@ const {
   serviceGetUsers,
   serviceGetBooksByUsername,
   serviceGetFriendsByUsername,
+  serviceGetWishListByUsername,
 } = require("../service/users.service");
 
 exports.controllerGetUsers = async (request, response, next) => {
@@ -29,6 +30,16 @@ exports.controllerGetFriendsByUsername = async (request, response, next) => {
     const { username } = request.params;
     const usersFriends = await serviceGetFriendsByUsername(username);
     response.status(200).send({ usersFriends });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.controllerGetWishListByUsername = async (request, response, next) => {
+  try {
+    const { username } = request.params;
+    const usersWishList = await serviceGetWishListByUsername(username);
+    response.status(200).send({ usersWishList });
   } catch (err) {
     next(err);
   }
