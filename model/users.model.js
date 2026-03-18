62798240c928292947631cf5fc18b4bd1da5bbaa
J-Mro <j.k.mroueh@gmail.com>
2026-03-18 09:58:41 +0000

    update getUserBookByIsbn to include JOIN in query

--- model/users.model.js
+++ model/users.model.js
@@ -41,7 +41,9 @@ exports.modelGetBooksByUsername = async (username) => {
 
 exports.modelGetUserBookByIsbn = async (username, isbn) => {
   const { rows } = await db.query(
-    `SELECT * FROM users_books WHERE isbn = $1 AND username = $2`,
+    `SELECT users_books.username, books.* FROM users_books
+     JOIN books ON users_books.isbn = books.isbn
+     WHERE users_books.isbn = $1 AND users_books.username = $2`,
     [isbn, username],
   );
   return rows;
